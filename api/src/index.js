d89cd18fcf17edbf18577c2696c4f4fd54b78165
Harryson Hu <harrysonhu@iMac-HarrysonHu.local>
2018-09-28 13:53:20 -0700

    Complete user authentication functionality

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import auth from "./routes/auth";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useNewUrlParser: true }
+);
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
