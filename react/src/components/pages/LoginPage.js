0298c954f8d6a871df0b58557482a028d0cef2c0
Harryson Hu <harrysonhu@iMac-HarrysonHu.local>
2018-09-28 10:36:17 -0700

    Renamed folders + initial commit for express backend

---
+++ react/src/components/pages/LoginPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import LoginForm from "../forms/LoginForm";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+
+class LoginPage extends Component {
+  state = {};
+
+  submit = data => {
+    return this.props.login(data).then(() => this.props.history.push("/"));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login page</h1>
+
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
