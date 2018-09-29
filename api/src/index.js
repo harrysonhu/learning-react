17a95e3c2453eead6a08a6a10d7d54f281f741e0
harrysonhu <harrysonh95@hotmail.com>
2018-09-29 11:36:09 -0700

    Implement user signup behavior

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
-import auth from "./routes/auth";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+
+import auth from "./routes/auth";
+import users from "./routes/users";
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,7 @@ mongoose.connect(
 );
 
 app.use("/api/auth", auth);
+app.use("/api/users", users);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
