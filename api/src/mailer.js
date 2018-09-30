f160b2462af821781676d80d7d40fdc0fe3335f4
harrysonhu <harrysonh95@hotmail.com>
2018-09-29 23:18:24 -0700

    Implement confirmation email and confirmation page

---
+++ api/src/mailer.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+const from = '"Learning React" <info@learningreact.com>';
+
+function setup() {
+  return nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+}
+
+export function sendConfirmationEmail(user) {
+  const transport = setup();
+  const email = {
+    from,
+    to: user.email,
+    subject: "Wecome to the app built to learn React",
+    text: `
+      Welcome to Learning React. Please, confirm your email.
+
+      ${user.generateConfirmationUrl()}
+      `
+  };
+
+  transport.sendMail(email);
+}
--- react/src/actions/auth.js
+++ react/src/actions/auth.js
@@ -20,3 +20,9 @@ export const logout = () => dispatch => {
   localStorage.removeItem("learningReactJWT");
   dispatch(userLoggedOut());
 };
+
+export const confirm = token => dispatch =>
+  api.user.confirm(token).then(user => {
+    localStorage.learningReactJWT = user.token;
+    dispatch(userLoggedIn(user));
+  });
