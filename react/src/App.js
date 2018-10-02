c2c46e973cd793dc818d29626e964f5bde958995
Harryson Hu <harrysonhu@iMac-HarrysonHu.local>
2018-10-02 13:56:05 -0700

    Implement dashboard nav bar and skeleton for new book page

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
@@ -9,11 +10,14 @@ import ConfirmationPage from "./components/pages/ConfirmationPage";
 import ResendConfirmationPage from "./components/pages/ResendConfirmationPage";
 import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import ResetPasswordPage from "./components/pages/ResetPasswordPage";
+import NewBookPage from "./components/pages/NewBookPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
+import TopNavigation from "./components/navigation/TopNavigation";
 
-const App = ({ location }) => (
+const App = ({ location, isAuthenticated }) => (
   <div className="ui container">
+    {isAuthenticated && <TopNavigation />}
     <Route location={location} path="/" exact component={HomePage} />
     <Route
       location={location}
@@ -52,13 +56,26 @@ const App = ({ location }) => (
       exact
       component={DashboardPage}
     />
+    <UserRoute
+      location={location}
+      path="/books/new"
+      exact
+      component={NewBookPage}
+    />
   </div>
 );
 
 App.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.email
+  };
+}
+
+export default connect(mapStateToProps)(App);
---
+++ react/src/components/pages/NewBookPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class NewBookPage extends Component {
+  state = {
+    books: null
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default NewBookPage;
