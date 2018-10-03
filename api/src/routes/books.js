ab2326aa438c622614737a6f00a47b5227570feb
Harryson Hu <harrysonhu@iMac-HarrysonHu.local>
2018-10-03 09:28:23 -0700

    Initial implementation of goodreads api

--- api/src/routes/books.js
+++ api/src/routes/books.js
@@ -1,7 +1,6 @@
 import express from "express";
 import authenticate from "../middlewares/authenticate";
-// FIX
-//import request from "request-promise";
+import request from "request-promise";
 import { parseString } from "xml2js";
 
 const router = express.Router();
@@ -14,32 +13,20 @@ router.get("/search", (req, res) => {
         req.query.q
       }`
     )
-    .then(result => res.json({ result }));
-
-  // res.json({
-  //   books: [
-  //     {
-  //       goodreadsId: "1",
-  //       title: "1984",
-  //       authors: "Orwell",
-  //       covers: [
-  //         "https://images.gr-assets.com/books/1348990566l/5470.jpg",
-  //         "https://images.gr-assets.com/books/1504611957l/9577857.jpg"
-  //       ],
-  //       pages: 198
-  //     },
-  //     {
-  //       goodreadsId: "2",
-  //       title: "Three Men in a Boat",
-  //       authors: "Jerome K. Jerome",
-  //       covers: [
-  //         "https://images.gr-assets.com/books/1392791656l/4921.jpg",
-  //         "https://images.gr-assets.com/books/1312036878l/627830.jpg"
-  //       ],
-  //       pages: 256
-  //     }
-  //   ]
-  // });
+    .then(result =>
+      parseString(result, (err, goodreadsResult) =>
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            work => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [work.best_book[0].small_image_url[0]]
+            })
+          )
+        })
+      )
+    );
 });
 
 export default router;
